153ab6616b9cf62e7b2bd9fc85eaa46ec2d3b011
rizkie24ti <rizkie24ti@mahasiswa.pcr.ac.id>
2025-06-26 13:25:42 +0700

    header.php

--- bioskop_app/includes/header.php
+++ bioskop_app/includes/header.php
@@ -16,6 +16,7 @@ include 'auth_check.php';
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Bioskop App</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="/bioskop_app/css/style.css">
 </head>
@@ -34,7 +35,7 @@ include 'auth_check.php';
                     <?php if (isset($_SESSION['user_id'])): ?>
                         <?php if (isset($_SESSION['role']) && $_SESSION['role'] === 'admin'): ?>
                             <li class="nav-item">
-                                <a class="nav-link" href="/bioskop_app/admin/">Admin Panel</a>
+                                <a class="nav-link" href="/bioskop_app/admin/">Menu Admin</a>
                             </li>
                         <?php endif; ?>
                         <li class="nav-item dropdown">
